2a06f8dcf22194eaa510806e964f571a3d98b822
RYunisov <runisov@gmail.com>
2012-09-19 12:07:57 +0400

    add method russian month

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module ApplicationHelper
   def logo
     logo = image_tag("rails.png", :alt => "Sample App", :class => "round") 
@@ -10,5 +11,15 @@ module ApplicationHelper
 	else
 	 "#{base_title} | #{@title}"
 	end
+  end
+
+  def rus_date(data)
+    d = data.split(%r{\D})
+    month = { 
+        "01" => "Янв", "02" => "Фев", "03" => "Мар", "04" => "Апр", "05" => "Май", "06" => "Июн",
+        "07" => "Июл", "08" => "Авг", "09" => "Сен", "10" => "Окт", "11" => "Ноя", "12" => "Дек" 
+    }
+    
+    return "#{d[2]+' '+month[d[1]]+' '+d[3]+':'+d[4]}"
   end
 end
